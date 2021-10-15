17c2d9ea0d640e9721efe17067816c2ffc320371
wujun <wujun@ddfox360.com>
2021-10-15 16:31:53 +0800

    e

--- src/store.js
+++ src/store.js
@@ -4,20 +4,20 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    tags: sessionStorage.tags,
-    isCollapse: false
-  },
-  mutations: {
-    TAG: (state, res) => {
-      state.tags = res
-      sessionStorage.tags = res
+    state: {
+        tags: sessionStorage.tags,
+        isCollapse: false
     },
-    IS_COLLAPSE: (state) => {
-      state.isCollapse = !state.isCollapse
+    mutations: {
+        TAG: (state, res) => {
+            state.tags = res
+            sessionStorage.tags = res
+        },
+        IS_COLLAPSE: (state) => {
+            state.isCollapse = !state.isCollapse
+        },
     },
-  },
-  actions: {
+    actions: {
 
-  }
+    }
 })
